1bc3cc0908f4400e90380c71a688a0441779315c
Bikash Katwal <bikashkatwal9@gmail.com>
2020-03-12 00:06:26 +1100

    added select options

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import apis from './../apis/ncovapi';
 
-export const fetchCases = () => async dispatch => {
-	const response = await apis.get(`Australia`);
+export const fetchCases = country => async dispatch => {
+	const response = await apis.get(`/cases?country=${country}`);
 	console.log(response);
 	dispatch({ type: 'FETCH_CASES', payload: response.data });
 };
--- src/components/VirusList.js
+++ src/components/VirusList.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { fetchCases } from '../actions';
+import CountrySelector from './CountrySelector';
 
 class VirusList extends Component {
 	componentDidMount() {
 		console.log(this.props);
-		this.props.fetchCases();
+		this.props.fetchCases('Australia');
 	}
 	render() {
 		return (
 			<div>
 				<h1>Virus List</h1>
+				<CountrySelector />
+				<button>Search</button>
 			</div>
 		);
 	}
